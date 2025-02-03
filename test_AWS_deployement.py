a8e89c23e3d37de501c2ccace59f54ffe390e4ff
adel-dabah <dabah.adel@gmail.com>
2025-02-03 11:17:29 +0100

    adding test_docker and test AWS_deployment

--- test_AWS_deployement.py
+++ test_AWS_deployement.py
@@ -1,6 +1,7 @@
 import requests
 
-url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+#url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+url ="https://f0dhzqffpk.execute-api.eu-north-1.amazonaws.com/test_stage/predict"
 data={'url' : 'https://github.com/adel-dabah/ml_capstone2/blob/main/dataset/Testing/pituitary/Te-piTr_0003.jpg?raw=true'}
 
 result= requests.post(url, json=data).json()
